f0364c244f9a2b34c01f0d86b609b786a26af255
Madhav Bondhare <bondharemadhav8@gmail.com>
2025-12-20 09:27:13 +0530

    new code added

--- BackEnd/Database/Database.js
+++ BackEnd/Database/Database.js
@@ -1,12 +1,20 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
-import { use } from 'react';
+import fs from 'fs';
 dotenv.config();
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database:process.env.MYSQL_DATABASE
+    port:process.env.MYSQL_PORT,
+    database:process.env.MYSQL_DATABASE,
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 }).promise();
 export async function InsertData(Name, Email, Password) {
     let [result] = await pool.query(`
