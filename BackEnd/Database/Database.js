4ad0209e106a6af48333458f2fa9ba9e7ce6cf5b
Madhav Bondhare <bondharemadhav8@gmail.com>
2025-12-28 19:17:16 +0530

    connect encrypted

--- BackEnd/Database/Database.js
+++ BackEnd/Database/Database.js
@@ -3,24 +3,24 @@ import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
 dotenv.config();
-// const ca = Buffer.from(
-//   process.env.CA,
-//   "base64"
-// );
+const ca = Buffer.from(
+  process.env.CA,
+  "base64"
+);
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     port:process.env.MYSQL_PORT,
     database:process.env.MYSQL_DATABASE,
-    // ssl: {
-    //     ca,
-    //     minVersion: 'TLSv1.2',
-    //     rejectUnauthorized: true
-    // },
-    // waitForConnections: true,
-    // connectionLimit: 10,
-    // queueLimit: 0
+    ssl: {
+        ca,
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 }).promise();
 export async function InsertData(Name, Email, Password) {
     let [result] = await pool.query(`
