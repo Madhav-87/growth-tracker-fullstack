4f4fa70562a308cf81d7511d8b797accdafb2f76
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-01-08 08:50:26 +0530

    iframe tag modify

--- FrontEnd/src/components/ui/RenderComp.jsx
+++ FrontEnd/src/components/ui/RenderComp.jsx
@@ -4,7 +4,7 @@ export default function RenderComp() {
   return (
     <div style={{minHeight:"100vh"}}>
         <iframe 
-        src="https://study-planner-app-uon5.vercel.app/dashboard" 
+        src={`https://study-planner-app-uon5.vercel.app/dashboard/?v=${Date.now()}`}
         title="External React Site"
         style={{height:"100vh",width:"100vw",scrollbarWidth:"none"}}
       />
