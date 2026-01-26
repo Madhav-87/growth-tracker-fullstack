f9ec4a5e7fb8aa7ff47faf30e987e77b5dba625f
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-01-26 06:08:58 +0530

    Update backend logic and services

--- BackEnd/services/visionClient.js
+++ BackEnd/services/visionClient.js
@@ -1,6 +1,8 @@
 const vision=require("@google-cloud/vision");
-
-// Create a Vision API client using credentials
-const visionClient = new vision.ImageAnnotatorClient();
+const credentials = JSON.parse(
+  Buffer.from(process.env.GOOGLE_VISION_CREDENTIALS_BASE64, "base64").toString("utf-8")
+);
+// `Create a Vision API client using credentials
+const visionClient = new vision.ImageAnnotatorClient({ credentials });
 
 module.exports=visionClient;
