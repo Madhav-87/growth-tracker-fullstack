6bfd6e2ca0af14e1d45730f743ca7f06141d748f
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-01-26 06:25:41 +0530

    Update backend logic and services

--- BackEnd/Database/Database.js
+++ BackEnd/Database/Database.js
@@ -2,13 +2,24 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
+const ca = Buffer.from(
+  process.env.CA,
+  "base64"
+);
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     port:process.env.MYSQL_PORT,
     database:process.env.MYSQL_DATABASE,
+    ssl: {
+        ca,
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 }).promise();
  async function InsertData(Name, Email, Password) {
     let [result] = await pool.query(`
