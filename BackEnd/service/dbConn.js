49eaa4c7d5c6c19295a2cf3b6f42c2a23bae100c
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-04-26 10:08:24 +0530

    Refactor Code : Database Connection Logic
    - Add Folder service
    - Add File dbConn.js
    - Move Database connection logic to dbConn.js

---
+++ BackEnd/service/dbConn.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const ca = Buffer.from(
+  process.env.CA,
+  "base64"
+);
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    port:process.env.MYSQL_PORT,
+    database:process.env.MYSQL_DATABASE,
+    ssl: {
+        ca,
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+}).promise();
+
+module.exports=pool;
