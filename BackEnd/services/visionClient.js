1b349065ddb659e714b9eb0900887d36bc426d5e
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-06-16 17:41:20 +0530

    Delete BackEnd directory

--- BackEnd/services/visionClient.js
+++
@@ -1,7 +0,0 @@
-const vision=require("@google-cloud/vision");
-const credentials = JSON.parse(
-  Buffer.from(process.env.GOOGLE_VISION_CREDENTIALS_BASE64, "base64").toString("utf-8")
-);
-const visionClient = new vision.ImageAnnotatorClient({ credentials });
-
-module.exports=visionClient;
