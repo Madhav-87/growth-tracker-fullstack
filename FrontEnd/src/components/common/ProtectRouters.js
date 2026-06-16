7d7acdce850dd6b05a4cb225dbc419ef51924706
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-06-16 17:41:39 +0530

    Delete FrontEnd directory

--- FrontEnd/src/components/common/ProtectRouters.js
+++
@@ -1,6 +0,0 @@
-import {Navigate} from 'react-router-dom';
-const ProtectRouters=({element})=>{
-    const token=localStorage.getItem('token');
-    return token?element:<Navigate to="/"/>
-}
-export default ProtectRouters;
