0d2f135c5e29f4760c1853235be53d1df92e1e8a
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-06-17 01:37:55 +0530

    add middleware : helmet

--- server/server.js
+++ server/server.js
@@ -3,13 +3,20 @@ const app=express();
 const cors=require('cors');
 const router=require('./router/routes.js');
 const dotenv=require('dotenv');
+const helmet =require('helmet');
 
 dotenv.config();
 app.use(cors());
+app.use(helmet());
 app.use(express.json({limit:"20mb"}));
+
 
 app.use('/',router);
 
+app.use ((err,req,res,next)=>{
+    console.log(err.stack);
+    res.status(err.status || 500).json({message:err.message || "Internal Server Error", success:false});
+})
 app.listen(process.env.PORT,()=>{
     console.log(`Server listening on port:${process.env.PORT}`);
 })
